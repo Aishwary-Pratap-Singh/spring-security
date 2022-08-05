56c279912d4116f8c2823212b953e1dd2df9a6ec
Aishwary Pratap Singh <101543730+aishwary-deloitte@users.noreply.github.com>
2022-08-05 15:58:05 +0530

    configured security according to requirement

--- springsecsection2/src/main/java/com/example/springsecsection2/Config/ProjectSecurityConfig.java
+++ springsecsection2/src/main/java/com/example/springsecsection2/Config/ProjectSecurityConfig.java
@@ -7,12 +7,29 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig  {
-
+    /**
+     *
+     * /myAccount - secured
+     * /myBalance - secured
+     * /myCards - secured
+     * /myLoans - secured
+     * /notices - not secured
+     * /contact - not secured
+     *
+     */
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+//                .anyRequest().authenticated()
+                .antMatchers("/myAccount").authenticated()
+                .antMatchers("/myBalance").authenticated()
+                .antMatchers("/myCards").authenticated()
+                .antMatchers("/myLoans").authenticated()
+
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll()
+
                 .and()
                 .formLogin().and()
                 .httpBasic();
