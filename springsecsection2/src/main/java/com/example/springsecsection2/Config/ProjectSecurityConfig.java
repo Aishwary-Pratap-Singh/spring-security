c513a674ec2034f8cdabdbf057bfc9973c7ef9d3
Aishwary Pratap Singh <101543730+aishwary-deloitte@users.noreply.github.com>
2022-08-06 20:06:41 +0530

    added assets folder

--- springsecsection2/src/main/java/com/example/springsecsection2/Config/ProjectSecurityConfig.java
+++ springsecsection2/src/main/java/com/example/springsecsection2/Config/ProjectSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -51,14 +52,15 @@ public class ProjectSecurityConfig   {
 
     @Bean
     public InMemoryUserDetailsManager adminDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
+        UserDetails user = User
+                .withUsername("user")
                 .password("12345")
                 .roles("USER")
                 .build();
 
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+        UserDetails admin = User
+                .withUsername("admin")
                 .password("12345")
                 .roles("ADMIN")
                 .build();
@@ -68,6 +70,10 @@ public class ProjectSecurityConfig   {
         return new InMemoryUserDetailsManager(arr);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 
 
 
